c7b208bce976eeec4379375a3ef2a48f0d800fd0
Franco <francozvilling2014@hotmail.com>
2025-05-16 00:26:27 -0300

    portfolio mayo

--- src/componentes/misTrabajos/misTrabajos.jsx
+++ src/componentes/misTrabajos/misTrabajos.jsx
@@ -1,8 +1,43 @@
 import React from "react";
-import Carta from "../carta/carta";
+import Carta from "../carta/carta"; // <-- CORREGIDO: Usando tu ruta de importación
 import "./misTrabajos.css";
 
 function MisTrabajos() {
+    const proyectos = [
+        {
+            img: "/images/library.png",
+            titulo: "Ecommerce - Librería",
+            descripcion: "Esta es mi librería creada a partir de React.js, CSS, JavaScript y HTML.",
+            linkProyecto: "https://libreriaonlinezwilling.netlify.app/"
+        },
+        {
+            img: "/images/cartaweb.webp",
+            titulo: "Carta Restaurante Web",
+            descripcion: "Carta de restaurante creada con React.js, CSS y JavaScript.",
+            linkProyecto: "https://cartawebrestaurant.netlify.app/"
+        },
+        {
+            img: "/images/cartawebdelivery.webp",
+            titulo: "Carta de restaurante con delivery",
+            descripcion: "Carta de delivery creada con React.js, CSS y JavaScript.",
+            linkProyecto: "https://cartawebdelivery.netlify.app/"
+        },
+        {
+            img: "/images/hp.png",
+            titulo: "Test Harry Potter",
+            descripcion: "Test interactivo creado con HTML, CSS y JavaScript.",
+            linkProyecto: "http://localhost:3000/mistrabajos"
+        },
+        {
+            img: "/images/proyecto-libreria.webp",
+            titulo: "Ecommerce - Librería",
+            descripcion: "Esta es mi primer ecommerce de librería creada a partir de React.js, CSS, JavaScript y HTML.",
+            linkProyecto: "https://libreriaonlinezwilling.web.app/"
+        }
+        
+        
+    ];
+
     return (
         <section className="misTrabajos-section container-fluid">
             <div className="row justify-content-center">
@@ -10,28 +45,18 @@ function MisTrabajos() {
                     <h1 className="mis_trabajos_1 section-title text-center">MIS TRABAJOS</h1>
                 </div>
             </div>
-            <div className="row justify-content-center">
-                <div className="col-md-8 col-lg-6 col-xl-5 d-flex justify-content-center">
-                    <Carta
-                        img="/images/proyecto-libreria.webp"
-                        titulo="Ecommerce - Librería"
-                        descripcion="Esta es mi librería creada a partir de React.js, CSS, JavaScript y HTML."
-                        linkProyecto="https://libreriaonlinezwilling.web.app/"
-                    />
-                    <Carta
-                        img="/images/cartaweb.webp"
-                        titulo="Carta Restaurante Web"
-                        descripcion="Carta de restaurante creada con React.js, CSS y JavaScript."
-                        linkProyecto="https://cartawebrestaurant.netlify.app/"
-                    />
-                    <Carta
-                        img="/images/cartawebdelivery.webp"
-                        titulo="Carta Delivery Web"
-                        descripcion="Carta de delivery creada con React.js, CSS y JavaScript."
-                        linkProyecto="https://cartawebdelivery.netlify.app/"
-                    />
-                </div>
-                {/* Aquí podrías añadir más proyectos si los tienes, envolviendo cada <Carta /> en su propia <div className="col-md-8 col-lg-6 col-xl-5 d-flex justify-content-center"> o ajustando las clases col para que quepan más */}
+           
+            <div className="row justify-content-center g-2">
+                {proyectos.map((proyecto, index) => (
+                    <div key={index} className="col-12 col-md-6 col-lg-4 d-flex align-items-stretch">
+                        <Carta
+                            img={proyecto.img}
+                            titulo={proyecto.titulo}
+                            descripcion={proyecto.descripcion}
+                            linkProyecto={proyecto.linkProyecto}
+                        />
+                    </div>
+                ))}
             </div>
         </section>
     );
